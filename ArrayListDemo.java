b3ac9c9ed1b907f6619ffa7500c169685cfd35bd
Aditya Pratap Singh Chauhan <90275944+Aditya2086@users.noreply.github.com>
2022-07-28 20:38:36 +0530

    Update ArrayListDemo.java

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -1,10 +1,3 @@
-package list;
-
-/**The ArrayList class implements the List interface. 
- * It uses a dynamic array to store the duplicate element of different data types. 
- * The ArrayList class maintains the insertion order and is non-synchronized. 
- * The elements stored in the ArrayList class can be randomly accessed */
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
