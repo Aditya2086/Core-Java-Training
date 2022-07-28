a64acbfd5e696c526464de78e97b7df86af08ca8
Aditya Pratap Singh Chauhan <90275944+Aditya2086@users.noreply.github.com>
2022-07-28 20:46:40 +0530

    Update VectorDemo.java

--- VectorDemo.java
+++ VectorDemo.java
@@ -1,13 +1,6 @@
-package list;
-
 import java.util.Iterator;
 import java.util.Vector;
 
-/**
- * Vector uses a dynamic array to store the data elements. 
- * It is similar to ArrayList.  * However, It is synchronized 
- * and contains many methods that are not the part of Collection framework.
- * **/
 public class VectorDemo {
 	public static void main(String[] args) {
 		Vector<Integer> v = new Vector<>();
