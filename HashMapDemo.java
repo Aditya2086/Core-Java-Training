060f83b83c5dfa141d4b625f3561b1d6d7af9893
Aditya Pratap Singh Chauhan <90275944+Aditya2086@users.noreply.github.com>
2022-07-28 20:42:17 +0530

    Update HashMapDemo.java

--- HashMapDemo.java
+++ HashMapDemo.java
@@ -1,11 +1,3 @@
-package map;
-/**
- * HashMap provides the basic implementation of the Map interface of Java. 
- * It stores the data in (Key, Value) pairs. To access a value in a HashMap, 
- * we must know its key. HashMap uses a technique called Hashing. Hashing is a 
- * technique of converting a large String to a small String that represents the 
- * same String so that the indexing and search operations are faster. 
- * HashSet also uses HashMap internally.**/
 import java.util.HashMap;
 import java.util.Map;
 
