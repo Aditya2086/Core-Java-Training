4f4b0fecf83a6edaa5c283cbe0851759bdced2c1
Aditya Pratap Singh Chauhan <90275944+Aditya2086@users.noreply.github.com>
2022-07-28 20:44:22 +0530

    Update LinkedListDemo.java

--- LinkedListDemo.java
+++ LinkedListDemo.java
@@ -1,11 +1,3 @@
-package list;
-
-/**LinkedList implements the Collection interface. 
- * It uses a doubly linked list internally to store the elements. 
- * It can store the duplicate elements. 
- * It maintains the insertion order and is not synchronized. 
- * In LinkedList, the manipulation is fast because no shifting is required.**/
-
 import java.util.Iterator;
 import java.util.LinkedList;
 
